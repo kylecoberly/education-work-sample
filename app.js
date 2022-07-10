48013992c17053faf923a05b5bdeaee051da2d02
Kyle Coberly <kyle.coberly@gmail.com>
2022-07-10 11:27:53 -0600

    Add linting

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ addScrollBehavior("[href='#instruction']", "#instruction")
 addScrollBehavior("[href='#assessment']", "#assessment")
 addScrollBehavior("[href='#philosophy']", "#philosophy")
 
-function addScrollBehavior(linkSelector, targetSelector){
-  const $link = document.querySelector(linkSelector);
-  const $target = document.querySelector(targetSelector);
+function addScrollBehavior (linkSelector, targetSelector) {
+  const $link = document.querySelector(linkSelector)
+  const $target = document.querySelector(targetSelector)
 
   $link.addEventListener("click", event => {
     event.preventDefault()
     history.pushState({}, "", targetSelector)
     try {
-      $target.scrollIntoView({ behavior: "smooth" })
-    } catch (_){
+      $target.scrollIntoView({behavior: "smooth"})
+    } catch (_) {
       $target.scrollIntoView(true)
     }
   })
